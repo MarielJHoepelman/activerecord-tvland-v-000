4fc4510cb545c782cd3c457920920ad946647e40
Mariel Hoepelman <marielhoepelman@gmail.com>
2019-03-26 01:06:58 +0000

    Automatically backed up by Learn

--- app/models/character.rb
+++ app/models/character.rb
@@ -1,5 +1,3 @@
-#* Define a method in the `Character` class, `#say_that_thing_you_say`, using a given character's catchphrase.
-#A character belongs to an actor and belongs to a show. MH
 class Character < ActiveRecord::Base
   belongs_to :actor
   belongs_to :show
